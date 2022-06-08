40f7cf1b07d35ce338405511aa907da32f6c55da
fr3ncyx <102382602+fr3ncyx@users.noreply.github.com>
2022-06-08 14:59:28 +0200

    Update index.js

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ client.on("messageCreate", (message) => {
 
     if (message.content == "!team") {
         var embed = new Discord.MessageEmbed()
-            .setColor("RED")
+            .setColor("#ff0000")
             .setTitle("Jkaisen")
             .setDescription(`${message.author.username} ecco a te il link discord: https://discord.gg/Pkhx88T3Z4 `)
             .setThumbnail("https://lh3.googleusercontent.com/MTyHWidhk1Uu0yHSZzoTpMUG4ZoCXOdhY2qqjcK8OOfsMh4yjAUF3uigIZ6XFZENH2DfDQ=s99")
