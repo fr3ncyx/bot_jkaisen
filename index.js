9fb17659f4bcf28fdee598956adb364fc11f2cf6
fr3ncyx <102382602+fr3ncyx@users.noreply.github.com>
2022-06-08 14:54:24 +0200

    Update index.js

--- index.js
+++ index.js
@@ -42,7 +42,8 @@ client.on("messageCreate", (message) => {
             .setTitle("Jkaisen")
             .setDescription(`${message.author.username} ecco a te il link discord: https://discord.gg/Pkhx88T3Z4 `)
             .setThumbnail("https://lh3.googleusercontent.com/MTyHWidhk1Uu0yHSZzoTpMUG4ZoCXOdhY2qqjcK8OOfsMh4yjAUF3uigIZ6XFZENH2DfDQ=s99")
-            .timestamp()
+            .setTimestamp()
+            
         message.channel.send({embeds: [embed] })
     }
 })
