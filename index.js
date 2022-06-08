1b600e365d3c528792e1dbbb123c4668020ba679
fr3ncyx <102382602+fr3ncyx@users.noreply.github.com>
2022-06-08 09:12:37 +0200

    Upload files

---
+++ index.js
@@ -0,0 +1,33 @@
+const Discord = require("discord.js")
+const client = new Discord.Client(
+    { intents:["GUILDS", "GUILD_MEMBERS", "GUILD_MESSAGES"] }
+)
+
+client.login("OTgxNjI0MDk0Mzg0NjExMzQ4.GdTaRx.NAa_g7eNJQrPBCZ6_iY1M_pOjXU2HBp844E4mU")
+
+client.on("ready", () => {
+    console.log("Bot online")
+})
+
+client.on("messageCreate", (message) => {
+    if (message.content == "!ciao") {
+        message.channel.send("ciao anche a te")
+    }
+
+    if (message.content == "!embed") {
+        var embed = new Discord.MessageEmbed()
+            .setTitle("Titolo embed")
+            .setDescription(`${message.author.username} ha scritto il messaggio`)
+            .setThumbnail("https://tuttoinformatico.com/wp-content/uploads/2019/10/call-of-duty-mobile-raggiunge-i-100-milioni-di-download.jpg")
+
+        message.channel.send({embeds: [embed] })
+    }
+
+    if (message.content == "!team") {
+        var embed = new Discord.MessageEmbed()
+            .setTitle("Jkaisen")
+            .setDescription(`${message.author.username} ecco a te il link discord: https://discord.gg/Pkhx88T3Z4 `)
+            .setThumbnail("https://lh3.googleusercontent.com/MTyHWidhk1Uu0yHSZzoTpMUG4ZoCXOdhY2qqjcK8OOfsMh4yjAUF3uigIZ6XFZENH2DfDQ=s99")
+        message.channel.send({embeds: [embed] })
+    }
+})
