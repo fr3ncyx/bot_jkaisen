1f1533a92ac17069dc08e941984c9402332d81cc
fr3ncyx <102382602+fr3ncyx@users.noreply.github.com>
2022-06-08 18:27:44 +0200

    Update index.js

--- index.js
+++ index.js
@@ -54,7 +54,7 @@ function oraAttuale(){
     var minutes  = new Date().getMinutes()
 
     var canale = client.channels.cache.get("956662480875159646")
-    if(hour == 15 && minutes == 46 ) {
+    if(hour == 20 && minutes == 30 ) {
        canale.send("Notifica")
     }
 }
