14b071f3036dd9658295b13be094676f8bb8fb82
fr3ncyx <102382602+fr3ncyx@users.noreply.github.com>
2022-06-09 16:03:05 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ const client = new Discord.Client(
     { intents:["GUILDS", "GUILD_MEMBERS", "GUILD_MESSAGES", "GUILD_SCHEDULED_EVENTS", "GUILD_PRESENCES", "GUILD_PRESENCES", "GUILD_EMOJIS_AND_STICKERS","GUILD_EMOJIS_AND_STICKERS", "DIRECT_MESSAGES", "DIRECT_MESSAGES"]}
 )
 
-client.on("message", message => {
+client.on("messageCreate", (message) => {
     if (message.content.startsWith("!userinfo2")) {
         if (message.content == "!userinfo2") {
-            var utente = message.member;
+             utente = message.member;
         }
         else {
-            var utente = message.mentions.members.first();
+             utente = message.mentions.members.first();
         }
 
         if (!utente) {
@@ -22,7 +22,7 @@ client.on("message", message => {
             elencoPermessi = "👑 ADMINISTRATOR";
         }
         else {
-            var permessi = ["CREATE_INSTANT_INVITE", "KICK_MEMBERS", "BAN_MEMBERS", "MANAGE_CHANNELS", "MANAGE_GUILD", "ADD_REACTIONS", "VIEW_AUDIT_LOG", "PRIORITY_SPEAKER", "STREAM", "VIEW_CHANNEL", "SEND_MESSAGES", "SEND_TTS_MESSAGES", "MANAGE_MESSAGES", "EMBED_LINKS", "ATTACH_FILES", "READ_MESSAGE_HISTORY", "MENTION_EVERYONE", "USE_EXTERNAL_EMOJIS", "VIEW_GUILD_INSIGHTS", "CONNECT", "SPEAK", "MUTE_MEMBERS", "DEAFEN_MEMBERS", "MOVE_MEMBERS", "USE_VAD", "CHANGE_NICKNAME", "MANAGE_NICKNAMES", "MANAGE_ROLES", "MANAGE_WEBHOOKS", "MANAGE_EMOJIS"]
+             var permessi = ["CREATE_INSTANT_INVITE", "KICK_MEMBERS", "BAN_MEMBERS", "MANAGE_CHANNELS", "MANAGE_GUILD", "ADD_REACTIONS", "VIEW_AUDIT_LOG", "PRIORITY_SPEAKER", "STREAM", "VIEW_CHANNEL", "SEND_MESSAGES", "SEND_TTS_MESSAGES", "MANAGE_MESSAGES", "EMBED_LINKS", "ATTACH_FILES", "READ_MESSAGE_HISTORY", "MENTION_EVERYONE", "USE_EXTERNAL_EMOJIS", "VIEW_GUILD_INSIGHTS", "CONNECT", "SPEAK", "MUTE_MEMBERS", "DEAFEN_MEMBERS", "MOVE_MEMBERS", "USE_VAD", "CHANGE_NICKNAME", "MANAGE_NICKNAMES", "MANAGE_ROLES", "MANAGE_WEBHOOKS", "MANAGE_EMOJIS"]
 
             for (var i = 0; i < permessi.length; i++) {
                 if (utente.permissions.has(permessi[i])) {
@@ -36,7 +36,7 @@ client.on("message", message => {
             .setDescription("Tutte le info di questo utente")
             .setThumbnail(utente.user.avatarURL())
             .addField("User id", "```" + utente.user.id + "```", true)
-            .addField("Status", "```" + utente.user.presence.status + "```", true)
+            .addField("Status", utente.presence ? utente.presence.status : "offline", true)
             .addField("Is a bot?", utente.user.bot ? "```Yes```" : "```No```", true)
             .addField("Account created", "```" + utente.user.createdAt.toDateString() + "```", true)
             .addField("Joined this server", "```" + utente.joinedAt.toDateString() + "```", true)
