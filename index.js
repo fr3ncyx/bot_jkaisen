22d52fc7275538683c3895727511d692705309b1
fr3ncyx <102382602+fr3ncyx@users.noreply.github.com>
2022-06-09 15:19:59 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,49 +1,52 @@
 const Discord = require("discord.js")
 const client = new Discord.Client(
-    { intents:["GUILDS", "GUILD_MEMBERS", "GUILD_MESSAGES", "GUILD_SCHEDULED_EVENTS", "GUILD_PRESENCES", "GUILD_PRESENCES", "GUILD_EMOJIS_AND_STICKERS","GUILD_EMOJIS_AND_STICKERS", "DIRECT_MESSAGES", "DIRECT_MESSAGES"] }
+    { intents:["GUILDS", "GUILD_MEMBERS", "GUILD_MESSAGES", "GUILD_SCHEDULED_EVENTS", "GUILD_PRESENCES", "GUILD_PRESENCES", "GUILD_EMOJIS_AND_STICKERS","GUILD_EMOJIS_AND_STICKERS", "DIRECT_MESSAGES", "DIRECT_MESSAGES"]}
 )
 
-module.exports = {
-    name: "user-info",
-    data: {
-        name: "user-info",
-        description: "Get information about a member",
-        options: [
-            {
-                name: "member",
-                description: "The target @member",
-                type: "MEMBER",
-                required: false 
-            }
-        ]
-    },
-    async execute(interaction) {
-        let utente = interaction.options.getMember("member") || interaction.member
-        
-        let elencoPermessi = "";
+client.on("message", message => {
+    if (message.content.startsWith("!userinfo2")) {
+        if (message.content == "!userinfo2") {
+            var utente = message.member;
+        }
+        else {
+            var utente = message.mentions.members.first();
+        }
+
+        if (!utente) {
+            message.channel.send("Non ho trovato questo utente")
+            return
+        }
+
+        var elencoPermessi = "";
         if (utente.permissions.has("ADMINISTRATOR")) {
             elencoPermessi = "👑 ADMINISTRATOR";
         }
         else {
-            let permessi = ["CREATE_INSTANT_INVITE", "KICK_MEMBERS", "BAN_MEMBERS", "ADMINISTRATOR", "MANAGE_CHANNELS", "MANAGE_GUILD", "ADD_REACTIONS", "VIEW_AUDIT_LOG", "PRIORITY_SPEAKER", "STREAM", "VIEW_CHANNEL", "SEND_MESSAGES", "SEND_TTS_MESSAGES", "MANAGE_MESSAGES", "EMBED_LINKS", "ATTACH_FILES", "READ_MESSAGE_HISTORY", "MENTION_EVERYONE", "USE_EXTERNAL_EMOJIS", "VIEW_GUILD_INSIGHTS", "CONNECT", "SPEAK", "MUTE_MEMBERS", "DEAFEN_MEMBERS", "MOVE_MEMBERS", "USE_VAD", "CHANGE_NICKNAME", "MANAGE_NICKNAMES", "MANAGE_ROLES", "MANAGE_WEBHOOKS", "MANAGE_EMOJIS_AND_STICKERS", "USE_APPLICATION_COMMANDS", "REQUEST_TO_SPEAK", "MANAGE_THREADS", "CREATE_PUBLIC_THREADS", "CREATE_PRIVATE_THREADS", "USE_EXTERNAL_STICKERS", "SEND_MESSAGES_IN_THREADS", "START_EMBEDDED_ACTIVITIES"]
-            for (let i = 0; i < permessi.length; i++)
-                if (utente.permissions.has(permessi[i]))
-                    elencoPermessi += `- ${permessi[i]}\r`
+            var permessi = ["CREATE_INSTANT_INVITE", "KICK_MEMBERS", "BAN_MEMBERS", "MANAGE_CHANNELS", "MANAGE_GUILD", "ADD_REACTIONS", "VIEW_AUDIT_LOG", "PRIORITY_SPEAKER", "STREAM", "VIEW_CHANNEL", "SEND_MESSAGES", "SEND_TTS_MESSAGES", "MANAGE_MESSAGES", "EMBED_LINKS", "ATTACH_FILES", "READ_MESSAGE_HISTORY", "MENTION_EVERYONE", "USE_EXTERNAL_EMOJIS", "VIEW_GUILD_INSIGHTS", "CONNECT", "SPEAK", "MUTE_MEMBERS", "DEAFEN_MEMBERS", "MOVE_MEMBERS", "USE_VAD", "CHANGE_NICKNAME", "MANAGE_NICKNAMES", "MANAGE_ROLES", "MANAGE_WEBHOOKS", "MANAGE_EMOJIS"]
+
+            for (var i = 0; i < permessi.length; i++) {
+                if (utente.permissions.has(permessi[i])) {
+                    elencoPermessi += "- " + permessi[i] + "\r";
+                }
+            }
         }
-        let embed = new Discord.MessageEmbed()
+
+        var embed = new Discord.MessageEmbed()
             .setTitle(utente.user.tag)
             .setDescription("Tutte le info di questo utente")
-            .setThumbnail(utente.user.displayAvatarURL())
-            .addField("User id", utente.user.id, true)
-            .addField("Status", utente.presence ? utente.presence.status : "offline", true)
-            .addField("Is a bot?", utente.user.bot ? "Yes" : "No", true)
-            .addField("Account created", utente.user.createdAt.toDateString(), true)
-            .addField("Joined this server", utente.joinedAt.toDateString(), true)
-            .addField("Permissions", elencoPermessi, false)
-            .addField("Roles", utente.roles.cache.map(ruolo => ruolo.name).join("\r"), false)
-        await interaction.reply({ embeds: [embed] })
+            .setThumbnail(utente.user.avatarURL())
+            .addField("User id", "```" + utente.user.id + "```", true)
+            .addField("Status", "```" + utente.user.presence.status + "```", true)
+            .addField("Is a bot?", utente.user.bot ? "```Yes```" : "```No```", true)
+            .addField("Account created", "```" + utente.user.createdAt.toDateString() + "```", true)
+            .addField("Joined this server", "```" + utente.joinedAt.toDateString() + "```", true)
+            .addField("Permissions", "```" + elencoPermessi + "```", false)
+            .addField("Roles", "```" + utente.roles.cache.map(ruolo => ruolo.name).join("\r") + "```", false)
+
+        message.channel.send(embed)
+
     }
-}
+});
 
 client.login(process.env.token)
 
