5805b25be47d7db1db7f149638c6000fdc7da7d1
fr3ncyx <102382602+fr3ncyx@users.noreply.github.com>
2022-06-09 10:24:04 +0200

    Update index.js

--- index.js
+++ index.js
@@ -70,7 +70,7 @@ client.on("guildMemberRemove", member => {
     canale.setName("Members: " + member.guild.memberCount)
 });
 
-client.on("message", (message) => {
+client.on("messageCreate", (message) => {
     if (message.content.startsWith("!userinfo")){
         if(message.content == "!userinfo"){
             var utente = message.member
