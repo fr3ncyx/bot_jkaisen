5a236a64b2e503d881392c6c3b5acb1e21fb3c73
fr3ncyx <102382602+fr3ncyx@users.noreply.github.com>
2022-06-09 16:08:05 +0200

    Update index.js

--- index.js
+++ index.js
@@ -19,10 +19,10 @@ client.on("messageCreate", (message) => {
 
          elencoPermessi = "";
         if (utente.permissions.has("ADMINISTRATOR")) {
-            elencoPermessi = "👑 ADMINISTRATOR";
+            var elencoPermessi = "👑 ADMINISTRATOR";
         }
         else {
-              permessi = ["CREATE_INSTANT_INVITE", "KICK_MEMBERS", "BAN_MEMBERS", "MANAGE_CHANNELS", "MANAGE_GUILD", "ADD_REACTIONS", "VIEW_AUDIT_LOG", "PRIORITY_SPEAKER", "STREAM", "VIEW_CHANNEL", "SEND_MESSAGES", "SEND_TTS_MESSAGES", "MANAGE_MESSAGES", "EMBED_LINKS", "ATTACH_FILES", "READ_MESSAGE_HISTORY", "MENTION_EVERYONE", "USE_EXTERNAL_EMOJIS", "VIEW_GUILD_INSIGHTS", "CONNECT", "SPEAK", "MUTE_MEMBERS", "DEAFEN_MEMBERS", "MOVE_MEMBERS", "USE_VAD", "CHANGE_NICKNAME", "MANAGE_NICKNAMES", "MANAGE_ROLES", "MANAGE_WEBHOOKS", "MANAGE_EMOJIS"]
+              var permessi = ["CREATE_INSTANT_INVITE", "KICK_MEMBERS", "BAN_MEMBERS", "MANAGE_CHANNELS", "MANAGE_GUILD", "ADD_REACTIONS", "VIEW_AUDIT_LOG", "PRIORITY_SPEAKER", "STREAM", "VIEW_CHANNEL", "SEND_MESSAGES", "SEND_TTS_MESSAGES", "MANAGE_MESSAGES", "EMBED_LINKS", "ATTACH_FILES", "READ_MESSAGE_HISTORY", "MENTION_EVERYONE", "USE_EXTERNAL_EMOJIS", "VIEW_GUILD_INSIGHTS", "CONNECT", "SPEAK", "MUTE_MEMBERS", "DEAFEN_MEMBERS", "MOVE_MEMBERS", "USE_VAD", "CHANGE_NICKNAME", "MANAGE_NICKNAMES", "MANAGE_ROLES", "MANAGE_WEBHOOKS", "MANAGE_EMOJIS"]
 
             for (var i = 0; i < permessi.length; i++) {
                 if (utente.permissions.has(permessi[i])) {
@@ -32,13 +32,13 @@ client.on("messageCreate", (message) => {
         }
 
         var embed = new Discord.MessageEmbed()
-            .setTitle(utente.user.tag)
+            .setTitle(utente.tag)
             .setDescription("Tutte le info di questo utente")
             .setThumbnail(utente.user.avatarURL())
-            .addField("User id", "```" + utente.user.id + "```", true)
+            .addField("User id", "```" + utente.id + "```", true)
             .addField("Status", utente.presence ? utente.presence.status : "offline", true)
-            .addField("Is a bot?", utente.user.bot ? "```Yes```" : "```No```", true)
-            .addField("Account created", "```" + utente.user.createdAt.toDateString() + "```", true)
+            .addField("Is a bot?", utente.bot ? "```Yes```" : "```No```", true)
+            .addField("Account created", "```" + utente.createdAt.toDateString() + "```", true)
             .addField("Joined this server", "```" + utente.joinedAt.toDateString() + "```", true)
             .addField("Permissions", "```" + elencoPermessi + "```", false)
             .addField("Roles", "```" + utente.roles.cache.map(ruolo => ruolo.name).join("\r") + "```", false)
