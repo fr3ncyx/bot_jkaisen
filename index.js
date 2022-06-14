b4d3408e9f9322fbaa27627bc2e74424494c8675
fr3ncyx <102382602+fr3ncyx@users.noreply.github.com>
2022-06-14 18:51:55 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,52 +1,44 @@
 const Discord = require("discord.js")
-const client = new Discord.Client(
-    { intents:["GUILDS", "GUILD_MEMBERS", "GUILD_MESSAGES"] }
-)
-
+const client = new Discord.Client();
 client.login(process.env.token)
 
-client.on("messageCreate", (message) => {
-    if (message.content.startsWith("!userinfo2")) {
-        if (message.content == "!userinfo2") {
-            var utente = message.member
+client.on("message", (message) => {
+    
+})
+
+client.on("messageCreate", message => {
+    if (message.content.startsWith("!userinfo")) {
+        if (message.content == "!userinfo") {
+            let utente = message.member;
         }
         else {
-            var utente = message.mentions.members.first()
+            let utente = message.member.first();
         }
-
         if (!utente) {
-            message.channel.send("Non ho trovato questo utente")
-            return
+            return message.channel.send("Non ho trovato questo utente")
         }
-
-        var elencoPermessi = ""
+        let elencoPermessi = ""
         if (utente.permissions.has("ADMINISTRATOR")) {
             elencoPermessi = "👑 ADMINISTRATOR"
         }
         else {
-             var permessi = ["CREATE_INSTANT_INVITE", "KICK_MEMBERS", "BAN_MEMBERS", "MANAGE_CHANNELS", "MANAGE_GUILD", "ADD_REACTIONS", "VIEW_AUDIT_LOG", "PRIORITY_SPEAKER", "STREAM", "VIEW_CHANNEL", "SEND_MESSAGES", "SEND_TTS_MESSAGES", "MANAGE_MESSAGES", "EMBED_LINKS", "ATTACH_FILES", "READ_MESSAGE_HISTORY", "MENTION_EVERYONE", "USE_EXTERNAL_EMOJIS", "VIEW_GUILD_INSIGHTS", "CONNECT", "SPEAK", "MUTE_MEMBERS", "DEAFEN_MEMBERS", "MOVE_MEMBERS", "USE_VAD", "CHANGE_NICKNAME", "MANAGE_NICKNAMES", "MANAGE_ROLES", "MANAGE_WEBHOOKS"]
-
-            for (var i = 0; i < permessi.length; i++) {
-                if (utente.permissions.has(permessi[i])) {
-                    elencoPermessi += "- " + permessi[i] + "\r"
-                }
-            }
+            var permessi = ["CREATE_INSTANT_INVITE", "KICK_MEMBERS", "BAN_MEMBERS", "ADMINISTRATOR", "MANAGE_CHANNELS", "MANAGE_GUILD", "ADD_REACTIONS", "VIEW_AUDIT_LOG", "PRIORITY_SPEAKER", "STREAM", "VIEW_CHANNEL", "SEND_MESSAGES", "SEND_TTS_MESSAGES", "MANAGE_MESSAGES", "EMBED_LINKS", "ATTACH_FILES", "READ_MESSAGE_HISTORY", "MENTION_EVERYONE", "USE_EXTERNAL_EMOJIS", "VIEW_GUILD_INSIGHTS", "CONNECT", "SPEAK", "MUTE_MEMBERS", "DEAFEN_MEMBERS", "MOVE_MEMBERS", "USE_VAD", "CHANGE_NICKNAME", "MANAGE_NICKNAMES", "MANAGE_ROLES", "MANAGE_WEBHOOKS", "MANAGE_EMOJIS_AND_STICKERS", "USE_APPLICATION_COMMANDS", "REQUEST_TO_SPEAK", "MANAGE_THREADS", "CREATE_PUBLIC_THREADS", "CREATE_PRIVATE_THREADS", "USE_EXTERNAL_STICKERS", "SEND_MESSAGES_IN_THREADS", "START_EMBEDDED_ACTIVITIES"]
+            for (let i = 0; i < permessi.length; i++)
+                if (utente.permissions.has(permessi[i]))
+                    elencoPermessi += `- ${permessi[i]}\n`
         }
-
-        var embed = new Discord.MessageEmbed()
+        let embed = new Discord.MessageEmbed()
             .setTitle(utente.user.tag)
             .setDescription("Tutte le info di questo utente")
-            .setThumbnail(utente.user.avatarURL())
-            .addField("User id", "```" + utente.user.id + "```", true)
+            .setThumbnail(utente.user.displayAvatarURL())
+            .addField("User id", utente.user.id, true)
             .addField("Status", utente.presence ? utente.presence.status : "offline", true)
-            .addField("Is a bot?", utente.user.bot ? "```Yes```" : "```No```", true)
-            .addField("Account created", "```" + utente.user.createdAt.toDateString() + "```", true)
-            .addField("Joined this server", "```" + utente.joinedAt.toDateString() + "```", true)
-            .addField("Permissions", "```" + elencoPermessi + "```", false)
-            .addField("Roles", "```" + utente.roles.cache.map(ruolo => ruolo.name).join("\r") + "```", false)
-
-        message.channel.send({embeds: [embed] })
-
+            .addField("Is a bot?", utente.user.bot ? "Yes" : "No", true)
+            .addField("Account created", utente.user.createdAt.toDateString(), true)
+            .addField("Joined this server", utente.joinedAt.toDateString(), true)
+            .addField("Permissions", elencoPermessi)
+            .addField("Roles", utente.roles.cache.map(ruolo => ruolo.name).join("\n"))
+        message.channel.send({ embeds: [embed] })
     }
 })
 
@@ -115,38 +107,3 @@ client.on("guildMemberRemove", member => {
     canale.setName("Members: " + member.guild.memberCount)
 });
 
-client.on("messageCreate", message => {
-    if (message.content.startsWith("!userinfo")) {
-        if (message.content == "!userinfo") {
-            var utente = message.member
-        }
-        else {
-            var utente = message.mentions.members.first()
-        }
-        if (!utente) {
-            return message.channel.send("Non ho trovato questo utente")
-        }
-        var elencoPermessi = ""
-        if (utente.permissions.has("ADMINISTRATOR")) {
-            elencoPermessi = "👑 ADMINISTRATOR"
-        }
-        else {
-            var permessi = ["CREATE_INSTANT_INVITE", "KICK_MEMBERS", "BAN_MEMBERS", "ADMINISTRATOR", "MANAGE_CHANNELS", "MANAGE_GUILD", "ADD_REACTIONS", "VIEW_AUDIT_LOG", "PRIORITY_SPEAKER", "STREAM", "VIEW_CHANNEL", "SEND_MESSAGES", "SEND_TTS_MESSAGES", "MANAGE_MESSAGES", "EMBED_LINKS", "ATTACH_FILES", "READ_MESSAGE_HISTORY", "MENTION_EVERYONE", "USE_EXTERNAL_EMOJIS", "VIEW_GUILD_INSIGHTS", "CONNECT", "SPEAK", "MUTE_MEMBERS", "DEAFEN_MEMBERS", "MOVE_MEMBERS", "USE_VAD", "CHANGE_NICKNAME", "MANAGE_NICKNAMES", "MANAGE_ROLES", "MANAGE_WEBHOOKS", "MANAGE_EMOJIS_AND_STICKERS", "USE_APPLICATION_COMMANDS", "REQUEST_TO_SPEAK", "MANAGE_THREADS", "CREATE_PUBLIC_THREADS", "CREATE_PRIVATE_THREADS", "USE_EXTERNAL_STICKERS", "SEND_MESSAGES_IN_THREADS", "START_EMBEDDED_ACTIVITIES"]
-            for (let i = 0; i < permessi.length; i++)
-                if (utente.permissions.has(permessi[i]))
-                    elencoPermessi += `- ${permessi[i]}\n`
-        }
-        let embed = new Discord.MessageEmbed()
-            .setTitle(utente.user.tag)
-            .setDescription("Tutte le info di questo utente")
-            .setThumbnail(utente.user.displayAvatarURL())
-            .addField("User id", utente.user.id, true)
-            .addField("Status", utente.presence ? utente.presence.status : "offline", true)
-            .addField("Is a bot?", utente.user.bot ? "Yes" : "No", true)
-            .addField("Account created", utente.user.createdAt.toDateString(), true)
-            .addField("Joined this server", utente.joinedAt.toDateString(), true)
-            .addField("Permissions", elencoPermessi)
-            .addField("Roles", utente.roles.cache.map(ruolo => ruolo.name).join("\n"))
-        message.channel.send({ embeds: [embed] })
-    }
-})
