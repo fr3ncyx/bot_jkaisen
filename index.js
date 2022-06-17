4b1e07c7471ce90e1e7f7d7a96ecd8c924af55f0
fr3ncyx <102382602+fr3ncyx@users.noreply.github.com>
2022-06-17 19:17:02 +0000

    Update index.js

--- index.js
+++ index.js
@@ -117,10 +117,10 @@ client.on("messageCreate", (message) => {
             .setTitle("丨CP SHOP - THE DESTROYERSKAISEN丨")
             .setDescription("Qui puoi trovare CP (punti COD) a prezzi molto più bassi rispetto al listino base. Fino al 60% in meno. Se vuoi ordinare o ricevere assistenza, apri un ticket nel canale 𝐒𝐮𝐩𝐩𝐨𝐫𝐭.")
             .setThumbnail("https://lh3.googleusercontent.com/MEbR9yl4_wmq-XucAkAIh3IT01HhQ9-yw--pXZC1ztmU1ULj4Noy5qWlJAYetIiQlSUbkqk=s151")
-            .addField("Prezzo" , "1°> 3700CP - 20€", false)
-            .addField("Prezzo" , "2°> 7400CP - 35€", true)
-            .addField("Prezzo" , "3°> 11.000CP - 50€", false)
-            .addField("Prezzo" , "4°> 25.700CP - 100€", true)
+            .addField("Prezzo" , " 3700CP - 20€", false)
+            .addField("Prezzo" , " 7400CP - 35€", true)
+            .addField("Prezzo" , " 11.000CP - 50€", false)
+            .addField("Prezzo" , " 25.700CP - 100€", true)
 
         message.channel.send({embeds: [embed] })
     }
