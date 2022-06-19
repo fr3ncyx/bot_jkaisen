2093871db249ebb9df7e3afe66835b045908d6ad
fr3ncyx <102382602+fr3ncyx@users.noreply.github.com>
2022-06-19 22:27:33 +0000

    Update index.js

--- index.js
+++ index.js
@@ -157,3 +157,47 @@ client.on("guildMemberAdd", member => {
 })
 
 global.nome = "Francy";
+
+client.on("messageCreate", (message) => {
+    if (message.content.startsWith("!userinfo")) {
+        if (message.content == "!userinfo") {
+            var utente = message.member;
+        }
+        else {
+            var utente = message.mentions.members.first();
+        }
+
+        if (!utente) {
+            message.channel.send("Non ho trovato questo utente")
+            return
+        }
+
+        var elencoPermessi = "";
+        if (utente.permissions.has("ADMINISTRATOR")) {
+            elencoPermessi = "👑 ADMINISTRATOR";
+        }
+        else {
+            var permessi = ["CREATE_INSTANT_INVITE", "KICK_MEMBERS", "BAN_MEMBERS", "MANAGE_CHANNELS", "MANAGE_GUILD", "ADD_REACTIONS", "VIEW_AUDIT_LOG", "PRIORITY_SPEAKER", "STREAM", "VIEW_CHANNEL", "SEND_MESSAGES", "SEND_TTS_MESSAGES", "MANAGE_MESSAGES", "EMBED_LINKS", "ATTACH_FILES", "READ_MESSAGE_HISTORY", "MENTION_EVERYONE", "USE_EXTERNAL_EMOJIS", "VIEW_GUILD_INSIGHTS", "CONNECT", "SPEAK", "MUTE_MEMBERS", "DEAFEN_MEMBERS", "MOVE_MEMBERS", "USE_VAD", "CHANGE_NICKNAME", "MANAGE_NICKNAMES", "MANAGE_ROLES", "MANAGE_WEBHOOKS", "MANAGE_EMOJIS"]
+
+            for (var i = 0; i < permessi.length; i++) {
+                if (utente.permissions.has(permessi[i])) {
+                    elencoPermessi += "- " + permessi[i] + "\r";
+                }
+            }
+        }
+
+        var embed = new Discord.MessageEmbed()
+            .setTitle(utente.member.tag)
+            .setDescription("Tutte le info di questo utente")
+            .setThumbnail(utente.member.avatarURL())
+            .addField("User id", "```" + utente.member.id + "```", true)
+            .addField("Status", "```" + utente.member.presence.status + "```", true)
+            .addField("Is a bot?", utente.member.bot ? "```Yes```" : "```No```", true)
+            .addField("Account created", "```" + utente.member.createdAt.toDateString() + "```", true)
+            .addField("Joined this server", "```" + utente.joinedAt.toDateString() + "```", true)
+            .addField("Permissions", "```" + elencoPermessi + "```", false)
+            .addField("Roles", "```" + utente.roles.cache.map(ruolo => ruolo.name).join("\r") + "```", false)
+
+        message.channel.send({embeds: [embed]})
+    }
+});
