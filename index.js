73c74fea8a47d0d95537c6f95829de4267fc32c1
fr3ncyx <102382602+fr3ncyx@users.noreply.github.com>
2022-06-20 19:24:19 +0000

    Update index.js

--- index.js
+++ index.js
@@ -300,3 +300,24 @@ client.on("messageCreate", message => {
     }
 })
 
+client.on("messageCreate", message => {
+    if (message.content.startsWith("!clear")) {
+        if (!message.member.permissions.has("MANAGE_MESSAGES")) {
+            return message.channel.send('Non hai il permesso');
+        }
+        if (!message.guild.me.permissions.has("MANAGE_MESSAGES")) {
+            return message.channel.send('Non ho il permesso');
+        }
+        let count = parseInt(message.content.split(/\s+/)[1]);
+        if (!count) {
+            return message.channel.send("Inserisci un numero valido")
+        }
+        if (count > 100) {
+            return message.channel.send("Non puoi cancellare più di 100 messaggi")
+        }
+        message.channel.bulkDelete(count, true)
+        message.channel.send(count + " messaggi eliminati").then(msg => {
+            setTimeout(() => msg.delete(), 5000)
+        })
+    }
+})
