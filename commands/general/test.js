ba6336be82ead055a2c4a3d60aa9df5dd65058cf
fr3ncyx <102382602+fr3ncyx@users.noreply.github.com>
2022-06-20 13:17:51 +0000

    update

--- commands/general/test.js
+++ commands/general/test.js
@@ -4,7 +4,6 @@ module.exports = {
     name: "test1",
     description: "Comando di test1",
     aliases: ["prova", "testbot"],
-    onlyStaff: true,
     execute(message, args) {
         message.channel.send(nome);
     }
