b9011bee8294aba91560c8b1171ab1cd5f1e67a8
fr3ncyx <102382602+fr3ncyx@users.noreply.github.com>
2022-06-23 17:11:24 +0200

    Update index.js

--- index.js
+++ index.js
@@ -111,8 +111,6 @@ client.on("messageCreate", (message)  => {
     }
 })
 
-var partials = Discord.Client({partials: ["MESSAGE", "CHANNEL", "REACTION"]})
-
 client.on("messageReactionAdd", async function (messageReaction, user) {  
     if (user.bot) return 
 
