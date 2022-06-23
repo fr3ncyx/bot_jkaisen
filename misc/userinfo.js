da8a4c6eabae9a7d148e84f4a5a556677ef52d66
fr3ncyx <102382602+fr3ncyx@users.noreply.github.com>
2022-06-23 19:40:29 +0200

    up

---
+++ misc/userinfo.js
@@ -0,0 +1,27 @@
+const {MessageEmbed} = require("discord.js")
+const Commando = require("discord.js")
+
+module.exports = class UserInfoCommand extends Commando.Command {
+    constructor(client) {
+        super(client, {
+            name: 'userinfo',
+            group: 'misc',
+            memberName: 'userinfo',
+            descrtption: 'info riguardo questo utente'
+        })
+    }
+
+    run = async (message) => {
+      const {guild, channel} = message
+
+      const user = message.mentions.users.first() || message.member.user
+      const member = guild.member.cache.get(user.id)
+
+      const embed = new MessageEmbed().setAuthor(
+        `Info utente di ${user.username}`,
+        userdisplayAvatarURL()
+      )
+
+      channel.send(embed)
+    }  
+  }
