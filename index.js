2e0dc3710239a1448236b56ec64be01d8fdc1cef
fr3ncyx <102382602+fr3ncyx@users.noreply.github.com>
2022-06-24 20:05:49 +0200

    Update index.js

--- index.js
+++ index.js
@@ -14,6 +14,29 @@ client.on("guildMemberAdd", member => {
 client.on("ready", () => {
     console.log("Bot online");
 })
+client.on("messageCreate", message => {
+    if (message.content.startsWith("!ban")) {
+        let utente = message.mentions.members.first();
+        if (!message.member.permissions.has('BAN_MEMBERS')) {
+            return message.channel.send('Non hai il permesso');
+        }
+        if (!utente) {
+            return message.channel.send('Non hai menzionato nessun utente');
+        }
+        if (!utente.bannable) {
+            return message.channel.send('Io non ho il permesso');
+        }
+        utente.ban()
+            .then(() => {
+                let embed = new Discord.MessageEmbed()
+                    .setTitle(`${utente.user.username} bannato`)
+                    .setDescription(`Utente bannato da ${message.author.toString()}`)
+
+                message.channel.send({ embeds: [embed] })
+            })
+    }
+})
+
 
 client.on("messageCreate" , (message) => {
     if (message.content == "!teamtag") {
