e2cf6d56667d93b7a5f710d845814defea32e4b5
fr3ncyx <102382602+fr3ncyx@users.noreply.github.com>
2022-06-26 16:06:56 +0200

    Update index.js

--- index.js
+++ index.js
@@ -61,7 +61,7 @@ client.on("messageCreate", message => {
     }
 })
 client.on("messageCreate" , (message) => {
-    if (message.content == "!teamtag" + message.member.toString()) {
+    if (message.content == "!teamtag" + member.toString()) {
         message.channel.send("Djk (provvisorio)" + message.member.toString())
     }
 });
