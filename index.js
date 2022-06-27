870c77dbe20ea0539083013742ea67622fbe8bad
fr3ncyx <102382602+fr3ncyx@users.noreply.github.com>
2022-06-27 21:49:15 +0200

    Update index.js

--- index.js
+++ index.js
@@ -394,15 +394,3 @@ client.on("messageCreate", message => {
         message.channel.send({embeds: [embed]})
     }
 })
-
-client.on("messageUpdate", (oldMessage, newMessage) => {
-    if (newMessage.guild.id !== "991063216178212915")
-    var embed = new Discord.MessageEmbed()
-    .setTitle("Messaggio modificato")
-    .setDescription("Messaggio modificato da" + oldMessage.author.username)
-    .addField("Messaggio di prima", oldMessage)
-    .addField("Nuovo messaggio", newMessage)
-    .setColor("#ff0000")
-     
-    client.channels.cache.get("991061700851666994").send({embeds: [embed]})
-})
