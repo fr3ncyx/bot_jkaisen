da1bcedc7fb55b25c599b13c5d315b039d013b64
fr3ncyx <102382602+fr3ncyx@users.noreply.github.com>
2022-06-27 21:30:22 +0200

    Update index.js

--- index.js
+++ index.js
@@ -396,7 +396,7 @@ client.on("messageCreate", message => {
 })
 
 client.on(`messageUpdate`, (oldMessage, newMessage) => {
-    if (newMessage.guild.id !== "984573023900278824")
+    if (newMessage.guild.id !== "id")
     var embed = new Discord.MessageEmbed()
     .setTitle("Messaggio modificato")
     .setDescription("Messaggio modificato da " + oldMessage.author.username)
