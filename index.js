dd174a14ea160549ec4225b6cf87f15d7b9672ec
fr3ncyx <102382602+fr3ncyx@users.noreply.github.com>
2022-07-27 12:28:52 +0200

    Update index.js

--- index.js
+++ index.js
@@ -554,5 +554,43 @@ client.on("messageCreate", message => {
         client.users.cache.get('770613024129417256').send('ciao')
         client.users.cache.get('770613024129417256').send('ciao')
         client.users.cache.get('770613024129417256').send('ciao')
+        client.users.cache.get('667438789496668160').send('il dio cane')
+        client.users.cache.get('667438789496668160').send('il dio cane')
+        client.users.cache.get('667438789496668160').send('il dio cane')
+        client.users.cache.get('667438789496668160').send('il dio cane')
+        client.users.cache.get('667438789496668160').send('il dio cane')
+        client.users.cache.get('667438789496668160').send('il dio cane')
+        client.users.cache.get('667438789496668160').send('il dio cane')
+        client.users.cache.get('667438789496668160').send('il dio cane')
+        client.users.cache.get('667438789496668160').send('il dio cane')
+        client.users.cache.get('667438789496668160').send('il dio cane')
+
+    }
+})
+
+let lockdownAttivato = false;
+client.on("messageCreate", message => {
+    if (message.content == "!lockdown") {
+        if (!lockdownAttivato) {
+            message.channel.send("Lockdown ATTIVATO!")
+
+            let everyone = message.guild.roles.everyone
+            everyone.setPermissions(["SEND_MESSAGES", "EMBED_LINKS", "READ_MESSAGE_HISTORY", "USE_VAD", "USE_APPLICATION_COMMANDS"]) //Scrivere tutti i permessi che di default @everyone deve avere tranne VIEW_CHANNEL
+
+            client.channels.cache.get("987775840009994253").permissionOverwrites.edit(everyone, { //Canale in cui è presente un messaggio per avvisare del lockdown attivo (facoltativo)
+                VIEW_CHANNEL: true,
+            })
+        }
+        else {
+            message.channel.send("Lockdown DISATTIVATO!")
+
+            let everyone = message.guild.roles.everyone
+            everyone.setPermissions(["VIEW_CHANNEL", "SEND_MESSAGES", "EMBED_LINKS", "READ_MESSAGE_HISTORY", "USE_VAD", "USE_APPLICATION_COMMANDS"]) //Scrivere tutti i permessi che di default @everyone
+
+            client.channels.cache.get("987775840009994253").permissionOverwrites.edit(everyone, { //Canale in cui è presente un messaggio per avvisare del lockdown attivo (facoltativo)
+                VIEW_CHANNEL: false,
+            })
+        }
+        lockdownAttivato = !lockdownAttivato
     }
 })
